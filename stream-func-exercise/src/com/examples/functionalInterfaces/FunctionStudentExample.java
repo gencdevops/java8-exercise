1f00f112c18d479f07b50be60726c3394087f2bf
gencdevops <gencdevops@gmail.com>
2021-10-14 16:33:27 +0300

    GenericMethodfunctionalIntefaces

---
+++ stream-func-exercise/src/com/examples/functionalInterfaces/FunctionStudentExample.java
@@ -0,0 +1,33 @@
+package com.examples.functionalInterfaces;
+
+
+import com.examples.data.Student;
+import com.examples.data.StudentDataBase;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+
+public class FunctionStudentExample {
+
+    static Function<List<Student>, Map<String, Double>>  function = (students -> {
+
+        Map<String,Double> studentGradeMap = new HashMap<>();
+        students.forEach((student -> {
+
+            if(PredicateStudentExample.p1.test(student)){
+                studentGradeMap.put(student.getName(),student.getGpa());
+            }
+        }));
+
+        return studentGradeMap;
+
+    });
+
+    public static void main(String[] args) {
+
+        System.out.println(function.apply(StudentDataBase.getAllStudents()));
+
+    }
+}
